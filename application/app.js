74fff873f82d253efb0f2288f385210beaba43b5
Denis Jacquemin <denis.jacquemin@gmail.com>
2014-02-11 09:16:49 +0100

    test 1 pass

---
+++ application/app.js
@@ -0,0 +1,13 @@
+/* global angular */
+/* global _ */
+
+var hbApp = angular.module('hbApp', []);     //       'ngRoute'
+
+//myApp.config( function($routeProvider) {
+//
+//    $routeProvider
+//        .when('/HB_welcome-page', {
+//            templateUrl: 'app/basic/basic.html'
+//        })
+//
+//});
